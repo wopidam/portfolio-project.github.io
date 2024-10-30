7f01e23bc202592b806222fbdd9b42c4f42be921
mpow-edu <mpow1411@gmail.com>
2024-10-30 10:29:53 -0500

    updated title

--- index.php
+++ index.php
@@ -1,73 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Portfolio Final Project</title>
-    <link rel="stylesheet" href="./css/styles.css" />
-    <script
-      src="https://code.jquery.com/jquery-3.7.1.min.js"
-      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-      crossorigin="anonymous"
-    ></script>
-  </head>
-  <body>
-    <div class="wrapper">
-      <header>
-        <h1>Madi Powers</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="./index.html">Home</a>
-            </li>
-            <li>
-              <a href="./gallery.html">Gallery</a>
-            </li>
-            <li>
-              <a href="./music.html">Music</a>
-            </li>
-            <li>
-              <a href="./visual-art.html">Visual Art</a>
-            </li>
-            <li>
-              <a href="./contact.html">Contact</a>
-            </li>
-            <li>
-              <a href="./references.html">References</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main class="page-main">
-        <h2>Homepage</h2>
-        <div class="main-content">
-          <div class="homepage-main">
-            
-            <p>
-              St. Louis-based musician, web designer, and printmaker.
-              <br />
-              <div class="horn-picture">
-                <img src="images/Asset 3.svg" alt=""/>
-              </div>
-              I have studied horn and trumpet for over 10 years, but lately, I've st.
-              <br />My approach to synthesizer has been heavily influenced by
-              artists such as: Cecil Taylor, Wendy Carlos, Mort Garson, and Sun
-              Ra. In particular, Cecil Taylor's philosophy of fully-improvised
-              performance has struck a chord with me.
-              <br />
-              <!-- <br />  <img class="main-img" src="./images/Hammerhead-Shark-2954345104.jpeg" alt="Hammerhead-Shark-2954345104"> -->
-              <br />
-              In addition to music, I have spent the last few years teaching
-              myself photography and digital art, as well as developing my
-              skills in Adobe CS while working at print labs.
-            </p>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Madi Powers</title>
+  <link rel="stylesheet" href="./css/styles.css" />
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="wrapper">
+    <header>
+      <h1>Madi Powers</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="./index.html">Home</a>
+          </li>
+          <li>
+            <a href="./gallery.html">Gallery</a>
+          </li>
+          <li>
+            <a href="./music.html">Music</a>
+          </li>
+          <li>
+            <a href="./visual-art.html">Visual Art</a>
+          </li>
+          <li>
+            <a href="./contact.html">Contact</a>
+          </li>
+          <li>
+            <a href="./references.html">References</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="page-main">
+      <h2>Homepage</h2>
+      <div class="main-content">
+        <div class="homepage-main">
+
+          <p>
+            St. Louis-based musician, web designer, and printmaker.
+            <br />
+          <div class="horn-picture">
+            <img src="images/Asset 3.svg" alt="" />
           </div>
-          <!-- <aside>
+          I have studied horn and trumpet for over 10 years, but lately, I've st.
+          <br />My approach to synthesizer has been heavily influenced by
+          artists such as: Cecil Taylor, Wendy Carlos, Mort Garson, and Sun
+          Ra. In particular, Cecil Taylor's philosophy of fully-improvised
+          performance has struck a chord with me.
+          <br />
+          <!-- <br />  <img class="main-img" src="./images/Hammerhead-Shark-2954345104.jpeg" alt="Hammerhead-Shark-2954345104"> -->
+          <br />
+          In addition to music, I have spent the last few years teaching
+          myself photography and digital art, as well as developing my
+          skills in Adobe CS while working at print labs.
+          </p>
+        </div>
+        <!-- <aside>
                 Extra Stuff
             </aside> -->
-        </div>
-      </main>
-      <footer>&copy;2024 Madi Powers</footer>
-    </div>
-  </body>
+      </div>
+    </main>
+    <footer>&copy;2024 Madi Powers</footer>
+  </div>
+</body>
+
 </html>
